14b82fed06ab4865a7f66ece61e7f88cac977654
veronica <timurkkq@gmail.com>
2025-09-06 13:23:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Vakhrusheva_Alina_ISP_233_Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    #1
+            //string a, g;
+            //int b;
+            //double f;
+            //Console.Write($"Введите ваше любимое кино: ");
+            //a= Console.ReadLine();
+            //Console.Write($"Ваша любимая цифра: ");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"Значение числа пи: ");
+            //f = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"Ваша любимая буква алфавита: ");
+            //g = Console.ReadLine();
+
+            //Console.WriteLine($"Ваше любимое кино-{a}  Ваша любимая цифра-{b}  Значение числа пи-{f} Ваша любимая буква алфавита-g");
+
+
+            //  #2
+            //string a = "I need more power!";
+            //Console.WriteLine(a);
+            //Console.WriteLine(a);
+            //Console.WriteLine(a);
+            //Console.WriteLine(a);
+
+            //  #3
+            //Console.WriteLine("\"Hello There\"");
+
+
+            
+            //#4
+            //int a, d, c, g, s;
+            //a = Convert.ToInt32(Console.ReadLine());
+            //d = Convert.ToInt32(Console.ReadLine());
+            //c = Convert.ToInt32(Console.ReadLine());
+            //g = Convert.ToInt32(Console.ReadLine());
+            //s = a + d + c + g;
+            //Console.WriteLine(s * 3);
+
+
+        }
+    }
+}
