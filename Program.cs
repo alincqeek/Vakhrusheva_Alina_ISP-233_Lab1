377f639894ec5da2bbb8556d0a505ad2521d3407
alina <a72658695@gmail.com>
2025-09-08 00:12:51 +0300

    Лаба 1

--- Program.cs
+++ Program.cs
@@ -31,8 +31,8 @@
             //Console.WriteLine("\"Hello There\"");
 
 
-            
-            //#4
+
+            // #4
             //int a, d, c, g, s;
             //a = Convert.ToInt32(Console.ReadLine());
             //d = Convert.ToInt32(Console.ReadLine());
@@ -41,7 +41,33 @@
             //s = a + d + c + g;
             //Console.WriteLine(s * 3);
 
+            // #5
+            //Console.Write("Введите 1 число: ");
+            //int a=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите 2 число: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //double d = 3 * Math.Pow(a+b,3)+ 275*Math.Pow(b, 3)-41;
+            //Console.WriteLine($"3(а+б)^3 +275b^2 - 127а-41 = {d} ");
 
+            // #6
+            //Console.Write("Введите имя: ");
+            //string a=Console.ReadLine();
+            //Console.Write("Введите возраст: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Привет, {a}!");
+            //Console.WriteLine($"Сейчас тебе {b} лет.");
+            //Console.WriteLine($"В следующем году тебе будет {b+1}.");
+
+            // #7
+            //string a = "Алина", c = "Волжский";
+            //int b = 17;
+            //Console.WriteLine($"Привет, {a}! Тебе {b} лет и ты живёшь в городе {c}.");
+
+            // #8
+            //Console.Write("Введите температуру в Цельсиях: ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Температура: {b*1.8+32}°F");
+            
         }
     }
 }
